ead3d0c40dd0efc20237168a2c60b9aed8bd6cab
Chris Evans_Laptop <evansc@mssm.org>
2017-09-14 19:56:38 -0400

    added csv file stuff

--- server.js
+++ server.js
@@ -1,21 +1,45 @@
-var url = require('url-parse');
 var express = require('express');
 var app = express();
-var url = require('url-parse');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var cookieParser = require('cookie-parser');
+var csvjson = require('csvjson');
 var fs = require('fs');
-var sanitizer = require("sanitizer");
+var path = require("path");
+var bodyParser = require('body-parser');
 
-
-var obj = JSON.parse(fs.readFileSync('studentdir.json', 'utf8'));
-var log = JSON.parse(fs.readFileSync('log.json', 'utf8'));
+var students = csvjson.toObject(fs.readFileSync(path.join(__dirname, 'StudentList.csv'), { encoding : 'utf8'}), {
+  delimiter : ',', // optional
+  quote     : '"' // optional
+});
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+app.get("/studentList", function(req, res){
+  res.send(students);
+});
 
+app.post("/sign", function(req, res){
+  var student = req.body.student;
+  var status = req.body.status;
+  if(status == "out"){
+    var loc = req.body.loc;
+    students[student].status = false;
+    students[student].loc = loc;
+  }else if(status == "in"){
+    students[student].status = true;
+    students[student].loc = "Dorm";
+  }
+
+  fs.writeFile("StudentList.csv", students, 'utf8', function (err) {
+      if (err) {
+          return console.log(err);
+      }
+      console.log("Student list updated");
+  });
+
+});
+
+
+console.log(students);
 
 app.use(express.static('public'));
 
@@ -24,5 +48,4 @@ var server = app.listen(8081, function() {
   require('dns').lookup(require('os').hostname(), function (err, add, fam) {
     console.log('Example app listening at http://%s:%s', add, port);
   })
-  fs.appendFileSync("logfile.log", "Started server on port " + port + " at " + getTime() + " on " + getDate() + "\n\n\n");
 });

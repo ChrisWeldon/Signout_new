56f13c0718ae1f019db6b3420169316e3f6c7794
Chris Evans_Laptop <evansc@mssm.org>
2017-09-20 14:00:01 -0400

    sorting names

--- server.js
+++ server.js
@@ -9,37 +9,61 @@ var students = csvjson.toObject(fs.readFileSync(path.join(__dirname, 'StudentLis
   delimiter : ',', // optional
   quote     : '"' // optional
 });
+var studentsId = {};
+makeIds();
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-app.get("/studentList", function(req, res){
-  res.send(students);
+app.get("/studentList/:sort", function(req, res){
+  var sendObj = {}
+  var sort = req.params.sort;
+  for (var key in students) {
+    if (students.hasOwnProperty(key)) {
+      if(students[key].first.toLowerCase().indexOf(sort) > -1 && students[key].first.toLowerCase().indexOf(sort) == 0 ){
+        console.log(students[key].first);
+        sendObj[students[key].id] = students[key];
+      }
+    }
+  }
+  res.send(sendObj);
 });
 
-app.post("/sign", function(req, res){
-  var student = req.body.student;
-  var status = req.body.status;
+app.get("/sign/:studentId/:state/:loc", function(req, res){
+  var student = req.params.studentId;
+  var status = req.params.state;
+  var loc = req.params.loc;
   if(status == "out"){
-    var loc = req.body.loc;
-    students[student].status = false;
-    students[student].loc = loc;
+    studentsId[student].status = false;
+    studentsId[student].location = loc;
   }else if(status == "in"){
-    students[student].status = true;
-    students[student].loc = "Dorm";
+    studentsId[student].status = true;
+    studentsId[student].location = "Dorm";
   }
+  //updateLocalSave();
+  res.send(studentsId[req.params.studentId]);
+});
 
+
+function updateLocalSave(){
   fs.writeFile("StudentList.csv", students, 'utf8', function (err) {
       if (err) {
           return console.log(err);
       }
       console.log("Student list updated");
   });
+}
 
-});
-
-
-console.log(students);
+function makeIds(){  //this should be in the csv file but I am making it procedurally cause its easier for now
+  for(var i in students){
+    if(students.hasOwnProperty(i)){
+      var id = students[i].last.toLowerCase() + students[i].first[0].toLowerCase();
+      studentsId[id] = students[i];
+      studentsId[id].id = id;
+    }
+  }
+  console.log(studentsId);
+}
 
 app.use(express.static('public'));
 
